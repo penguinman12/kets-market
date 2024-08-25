4a3e7bb744ad43bf257830a76256dfdd9fbbe95b
KM <penguinman12@snu.ac.kr>
2024-08-26 03:14:10 +0900

    work 4

--- accountapp/models.py
+++ accountapp/models.py
@@ -1,5 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
 
 
+class Account(models.Model):
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='account')
+    carbon_credits = models.PositiveIntegerField(default=500)  # 초기 탄소 배출량
+    cash = models.DecimalField(max_digits=10, decimal_places=2, default=5000.00)  # 초기 현금 잔액
