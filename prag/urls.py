d9c6db500cbd041aa91a0ac19be8ea6d9a41927a
KM <penguinman12@snu.ac.kr>
2024-08-25 14:32:39 +0900

    3 fix

--- prag/urls.py
+++ prag/urls.py
@@ -14,9 +14,23 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from articleapp.views import ArticleListView
 
 urlpatterns = [
+    path('', ArticleListView.as_view(), name='home'),
+
     path('admin/', admin.site.urls),
-]
+    path('accounts/', include('accountapp.urls')),
+    path('profiles/', include('profileapp.urls')),
+    path('articles/', include('articleapp.urls')),
+    path('comments/', include('commentapp.urls')),
+    path('projects/', include('projectapp.urls')),
+    path('subscribe/', include('subscribeapp.urls')),
+    path('likes/', include('likeapp.urls')),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
