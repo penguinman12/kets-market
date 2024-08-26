db533cb1bcfa200ea3902ea1ab25190d7c3cbe83
KM <penguinman12@snu.ac.kr>
2024-08-26 15:33:01 +0900

    FIX FIX 2

--- prag/settings/deploy.py
+++ prag/settings/deploy.py
@@ -1,5 +1,12 @@
 from .base import *
 
+
+def read_secret(secret_name):
+    file = open('/run/secrets/' + secret_name)
+    secret = file.read()
+    secret = secret.rstrip().lstrip()
+    file.close()
+    return secret
 env = environ.Env(
     #set casting, default value
     DEBUG = (bool,False)
@@ -16,7 +23,7 @@ environ.Env.read_env(
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret! 배포 시 노출 X
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = read_secret('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -31,7 +38,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.mysql",
         "NAME": "django",
         "USER": "django",
-        "PASSWORD": "password1234",
+        "PASSWORD": read_secret('MYSQL_PASSWORD'),
         "HOST": "mariadb",
         "PORT": "3306",
     }
