06330bcd40aaae178bf9340e025430020d57d044
KM <penguinman12@snu.ac.kr>
2024-08-26 12:32:35 +0900

    now being dead

--- prag/settings/deploy.py
+++ prag/settings/deploy.py
@@ -1,41 +1,38 @@
-import environ
-
 from .base import *
 
-
-
 env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False)
+    #set casting, default value
+    DEBUG = (bool,False)
 )
 
-# reading .env file
+
+
+#reading .env file
 environ.Env.read_env(
     env_file= os.path.join(BASE_DIR, '.env')
 )
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('DJANGO_SECRET_KEY')
+# SECURITY WARNING: keep the secret key used in production secret! 배포 시 노출 X
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = ['*'] #모든 사용자 사용 가능
 
 # Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django',
-        'USER': 'django',
-        'PASSWORD': 'password1234',
-        'HOST': 'mariadb',
-        'PORT': '3306',
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "django",
+        "USER": "django",
+        "PASSWORD": "password1234",
+        "HOST": "mariadb",
+        "PORT": "3306",
     }
 }
--- prag/settings/local.py
+++ prag/settings/local.py
@@ -19,6 +19,8 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+
+
 ALLOWED_HOSTS = ['*']
 
 
