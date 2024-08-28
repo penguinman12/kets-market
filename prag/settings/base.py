b17c4056cbe7a968252d5f46fd6d956780da95ce
KM <penguinman12@snu.ac.kr>
2024-08-28 11:31:20 +0900

    pls woking.. pls

--- prag/settings/base.py
+++ prag/settings/base.py
@@ -99,11 +99,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'ko-kr'
+LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'Asia/seoul'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
+
+USE_L10N = True
 
 USE_TZ = True
 
